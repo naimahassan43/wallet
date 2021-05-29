5fdb85d9b61583f5e9d1939173e97aee904a3ef9
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-05-29 16:21:04 +0100

    Reset form and additem function

--- index.js
+++ index.js
@@ -6,25 +6,36 @@ document.querySelector('#ewallet-form').addEventListener('submit', function(e) {
     const desc = document.querySelector('.add__description').value;
     const value = document.querySelector('.add__value').value;
     // console.log(desc, type, value);
+    addItems(type, desc, value);
 
+    resetForm();
+});
+
+function addItems(type, desc, value) {
     const newHtml = `
-              <div class="item">
-                <div class="item-description-time">
-                    <div class="item-description">
-                        <p>${desc}</p>
-                    </div>
-                    <div class="item-time">
-                        <p>25 Feb, 06:45 PM</p>
-                    </div>
-                </div>
-                <div class="item-amount ${type === '+'?'income-amount':'expense-amount'}">
-                    <p>${type}$${value}</p>
-                </div>
-              </div>
-  `
+  <div class="item">
+    <div class="item-description-time">
+        <div class="item-description">
+            <p>${desc}</p>
+        </div>
+        <div class="item-time">
+            <p>25 Feb, 06:45 PM</p>
+        </div>
+    </div>
+    <div class="item-amount ${type === '+' ? 'income-amount' : 'expense-amount'}">
+        <p>${type}$${value}</p>
+    </div>
+  </div>
+`
         // console.log(newHtml);
 
     const collection = document.querySelector('.collection');
     collection.insertAdjacentHTML('afterbegin', newHtml);
 
-})
+}
+
+function resetForm() {
+    document.querySelector('.add__type').value = '+';
+    document.querySelector('.add__description').value = '';
+    document.querySelector('.add__value').value = '';
+}
